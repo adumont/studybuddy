fa552e1c360166fc5f48ad8997f364f8ea58368e
Alexandre Dumont <adumont@gmail.com>
2023-11-26 20:50:22 +0100

    Add chunk title, add icons

--- app.py
+++ app.py
@@ -40,10 +40,6 @@ class Question4Choices(BaseModel):
         return v
 
 
-corpus = loadp("GEOGRAFIA.pkl")
-
-order_permitations = get_order_permitations()
-
 def init_session_state(key: str, default: any = None):
     if key not in st.session_state:
         st.session_state[key] = default
@@ -52,14 +48,22 @@ def init_session_state(key: str, default: any = None):
 def set_session_state(key: str, value: any):
     st.session_state[key] = value
 
+def get_asignatura():
+    return st.session_state["asignatura_seleccionada"]
 
-init_session_state(key="current_chunk", default=0)
-init_session_state(key="current_question", default=0)
-init_session_state(key="current_answer", default=-1)
+# init_session_state(key="current_chunk", default=0)
+# init_session_state(key="current_question", default=0)
+# init_session_state(key="current_answer", default=-1)
 init_session_state(key="ver_mas_info", default={"tema": -1, "question": -1})
+init_session_state(key="asignatura_seleccionada", default="GEOGRAFIA")
+
+asignatura = get_asignatura()
+corpus = loadp(f"{asignatura}.pkl")
+
+order_permitations = get_order_permitations()
 
 # Show lista de temas (chunks)
-chunk_list = [f"# Chunk {i}" for i in range(len(corpus["chunks"]))]
+chunk_list = [f"{i} - {c['title']}" for i, c in enumerate(corpus["chunks"])]
 
 if "user_responses" not in st.session_state:
     st.session_state["user_responses"] = []
@@ -73,9 +77,19 @@ if "user_responses" not in st.session_state:
 #     st.write(st.session_state["user_responses"])
 
 
-tema_seleccionado = st.selectbox(
-    label="tema", options=chunk_list, key="tema_seleccionado"
-)
+with st.sidebar:
+    st.write("Elige una asignatura:")
+    asignatura_seleccionada = st.selectbox(
+        label="Asignaturas:", options=["GEOGRAFIA"], key="asignatura_seleccionada"
+    )
+
+    st.write("Elige un tema en la lista siguiente:")
+    tema_seleccionado = st.selectbox(
+        label="Temas:", options=chunk_list, key="tema_seleccionado"
+    )
+    st.info(
+        "Aplicacion desarrollada por [Alexandre Dumont](https://adumont.github.io/)"
+    )
 
 tema_n = chunk_list.index(tema_seleccionado)
 
@@ -115,6 +129,8 @@ def ver_mas_info(tema_n, qn):
         st.session_state["ver_mas_info"] = {"tema": tema_n, "question": qn}
 
 
+st.write(f"""## {chunk["title"]}""")
+
 for qn, question in enumerate(chunk["questions"]):
     with st.expander(label=f"Question {qn}", expanded=True):
         st.write(f"""**{question["question"].question}**""")
@@ -125,10 +141,13 @@ for qn, question in enumerate(chunk["questions"]):
         # st.write(question["question"])
         # st.write(st.session_state["ver_mas_info"])
 
+        nums_icons = ["1️⃣", "2️⃣", "3️⃣", "4️⃣" ]
+
         for i, an in enumerate(order):
             answer = answers[an]
+            # one button for each possible answer
             st.button(
-                f"({i+1}) {answer.answer}",
+                f"{nums_icons[i]} {answer.answer}",
                 key=f"{qn}-{an}",
                 on_click=answer_clicked,
                 args=(tema_n, qn, an),
@@ -136,9 +155,9 @@ for qn, question in enumerate(chunk["questions"]):
             )
             if st.session_state["user_responses"][tema_n][qn]["user_answer"] == an:
                 if answer.is_correct:
-                    st.info(answer.explanation)
+                    st.info(f"✅ {answer.explanation}")
                 else:
-                    st.error(answer.explanation)
+                    st.error(f"⚠️ {answer.explanation}")
 
                 # Show more...
                 if st.session_state["ver_mas_info"] == {
@@ -146,13 +165,13 @@ for qn, question in enumerate(chunk["questions"]):
                     "question": qn,
                 }:
                     # user clicked the "Ver mas" button
-                    st.write(f"""Fact: **{question["fact"]}**""")
-                    st.write(f"""Lesson: {chunk["chunk"].page_content}""")
+                    st.write(f"""**Fact: {question["fact"]}**""")
+                    st.write(f"""**Lesson**: {chunk["chunk"].page_content}""")
 
                 else:
                     # Show "ver mas" button
                     st.button(
-                        label="Ver info",
+                        label="📒Leer mas...",
                         key=f"ver-mas-{tema_n}-{qn}",
                         on_click=ver_mas_info,
                         args=(tema_n, qn),
